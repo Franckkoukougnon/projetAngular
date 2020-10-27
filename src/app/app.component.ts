88ffb9e2e4b354e072056f77376f78488d8dbff1
franckkoukougnon <franckkoukougnon@gmail.com>
2020-10-27 23:35:34 +0100

    mon projet angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppareilService } from './services/appareil.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'myProject';
+export class AppComponent implements OnInit {
+  isAuth = false;
+
+  lastDate = new Date();
+
+  appareils: any[];
+
+
+
+
+
+  constructor(private appareilService: AppareilService) {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+
+  onAllume() {
+    this.appareilService.switchOnAll();
+  }
+
+  onEteint() {
+    this.appareilService.switchOffAll();
+  }
+
+
+  ngOnInit() {
+    this.appareils = this.appareilService.appareils;
+  }
 }
+
+
